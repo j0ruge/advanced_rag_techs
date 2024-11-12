005157699b5238660fe25cc5ccc4fe4b888b9481
j0ruge <joruge@gmail.com>
2024-11-12 11:52:12 -0300

    Ajustes de virtual env

--- expansion_answer.py
+++ expansion_answer.py
@@ -11,3 +11,19 @@ load_dotenv();
 openai_key = os.getenv("OPENAI_API_KEY");
 
 print(openai_key);
+
+reader = PdfReader("data/microsoft-annual-report.pdf");
+pdf_texts = [page.extract_text().strip() for page in reader.pages];
+
+# Filter the empty strings
+pdf_texts = [text for text in pdf_texts if text];
+
+#print(
+#     word_wrap(
+#         pdf_texts[0],
+#         width=100,
+#     )
+# );
+
+# split the text into smaller chunks
+# to avoid the 4096 token limit
