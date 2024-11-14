f2222101a75b914720359ecc1b6f677d16b453cf
j0ruge <joruge@gmail.com>
2024-11-13 23:35:59 -0300

    Plotting and Projecting the Embedded Results on Graph

--- expansion_answer.py
+++ expansion_answer.py
@@ -1,8 +1,9 @@
-from helper_utils import word_wrap;
+from helper_utils import project_embeddings, word_wrap;
 from pypdf import PdfReader;
 import os;
 from openai import OpenAI;
 from dotenv import load_dotenv;
+import umap;
 
 # Load the environment variables from the .env file
 load_dotenv();
@@ -126,6 +127,62 @@ results = chroma_collection.query(
 )
 retrieved_documents = results["documents"][0];
 
-for document in retrieved_documents:
-    print(word_wrap(document));
-    print("\n");
+# for document in retrieved_documents:
+#     print(word_wrap(document));
+#     print("\n");
+
+embeddings = chroma_collection.get(include=["embeddings"])["embeddings"];
+umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings);
+projected_dataset_embeddings = project_embeddings(embeddings, umap_transform);
+
+retrieved_embeddings = results["embeddings"][0];
+original_query_embedding = embedding_function([original_query]);
+augmented_query_embedding = embedding_function([joint_query]);
+
+projected_original_query_embedding = project_embeddings(
+    original_query_embedding, umap_transform
+);
+projected_augmented_query_embedding = project_embeddings(
+    augmented_query_embedding, umap_transform
+);
+projected_retrieved_embeddings = project_embeddings(
+    retrieved_embeddings, umap_transform
+);
+
+import matplotlib.pyplot as plt
+
+# Plot the projected query and retrieved documents in the embedding space
+plt.figure()
+
+plt.scatter(
+    projected_dataset_embeddings[:, 0],
+    projected_dataset_embeddings[:, 1],
+    s=10,
+    color="gray",
+)
+plt.scatter(
+    projected_retrieved_embeddings[:, 0],
+    projected_retrieved_embeddings[:, 1],
+    s=100,
+    facecolors="none",
+    edgecolors="g",
+)
+plt.scatter(
+    projected_original_query_embedding[:, 0],
+    projected_original_query_embedding[:, 1],
+    s=150,
+    marker="X",
+    color="r",
+)
+plt.scatter(
+    projected_augmented_query_embedding[:, 0],
+    projected_augmented_query_embedding[:, 1],
+    s=150,
+    marker="X",
+    color="orange",
+)
+
+plt.gca().set_aspect("equal", "datalim")
+plt.title(f"{original_query}")
+plt.axis("off")
+plt.show()  # display the plot
