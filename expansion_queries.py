3edd3fb14059126455ad018c4ec251d35d418e05
j0ruge <joruge@gmail.com>
2024-11-16 17:18:41 -0300

    expansion_querias

--- expansion_queries.py
+++ expansion_queries.py
@@ -68,3 +68,133 @@ query = "What was the total revenue for the year?"
 
 results = chroma_collection.query(query_texts=[query], n_results=5)
 retrieved_documents = results["documents"][0]
+
+# for document in retrieved_documents:
+#     print(word_wrap(document))
+#     print("\n")
+
+def generate_multi_query(query, model="gpt-4o-mini"):
+
+    prompt = """
+    You are a knowledgeable financial research assistant. 
+    Your users are inquiring about an annual report. 
+    For the given question, propose up to five related questions to assist them in finding the information they need. 
+    Provide concise, single-topic questions (withouth compounding sentences) that cover various aspects of the topic. 
+    Ensure each question is complete and directly related to the original inquiry. 
+    List each question on a separate line without numbering.
+                """
+
+    messages = [
+        {
+            "role": "system",
+            "content": prompt,
+        },
+        {   
+            "role": "user",
+            "content": query
+        },
+    ]
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+    content = response.choices[0].message.content
+    content = content.split("\n")
+    return content
+
+
+original_query = (
+    "What details can you provide about the factors that led to revenue growth?"
+)
+aug_queries = generate_multi_query(original_query)
+
+# 1. First step show the augmented queries
+for query in aug_queries:
+    print("\n", query)
+
+# 2. concatenate the original query with the augmented queries
+joint_query = [
+    original_query
+] + aug_queries  # original query is in a list because chroma can actually handle multiple queries, so we add it in a list
+
+# print("======> \n\n", joint_query)
+
+results = chroma_collection.query(
+    query_texts=joint_query, n_results=5, include=["documents", "embeddings"]
+)
+retrieved_documents = results["documents"]
+
+# Deduplicate the retrieved documents
+unique_documents = set()
+for documents in retrieved_documents:
+    for document in documents:
+        unique_documents.add(document)
+
+
+# output the results documents
+for index, documents in enumerate(retrieved_documents):
+    print(f"Query: {joint_query[index]}")
+    print("")
+    print("Results:")
+    for doc in documents:
+        print(word_wrap(doc))
+        print("")
+    print("-" * 100)
+
+
+embeddings = chroma_collection.get(include=["embeddings"])["embeddings"]
+umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings)
+projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+
+# 4. We can also visualize the results in the embedding space
+original_query_embedding = embedding_function([original_query])
+augmented_query_embeddings = embedding_function(joint_query)
+
+project_original_query = project_embeddings(original_query_embedding, umap_transform)
+project_augmented_queries = project_embeddings(
+    augmented_query_embeddings, umap_transform
+)
+
+retrieved_embeddings = results["embeddings"];
+result_embeddings = [item for sublist in retrieved_embeddings for item in sublist];
+
+projected_result_embeddings = project_embeddings(result_embeddings, umap_transform);
+
+import matplotlib.pyplot as plt
+
+
+# Plot the projected query and retrieved documents in the embedding space
+plt.figure()
+plt.scatter(
+    projected_dataset_embeddings[:, 0],
+    projected_dataset_embeddings[:, 1],
+    s=10,
+    color="gray",
+)
+plt.scatter(
+    project_augmented_queries[:, 0],
+    project_augmented_queries[:, 1],
+    s=150,
+    marker="X",
+    color="orange",
+)
+plt.scatter(
+    projected_result_embeddings[:, 0],
+    projected_result_embeddings[:, 1],
+    s=100,
+    facecolors="none",
+    edgecolors="g",
+)
+plt.scatter(
+    project_original_query[:, 0],
+    project_original_query[:, 1],
+    s=150,
+    marker="X",
+    color="r",
+)
+
+plt.gca().set_aspect("equal", "datalim")
+plt.title(f"{original_query}")
+plt.axis("off")
+plt.show()  # display the plot
